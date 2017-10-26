3d8b2fd78d6d05ac5d78a1a57742bc029c84ea49
SoarTheSky <32531272+SoarTheSky@users.noreply.github.com>
2017-10-27 00:24:07 +0700

    Initial Commit

---
+++ AlGeo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlGeo
+//
+//  Created by Rama R. on 27/10/17.
+//  Copyright © 2017 Tubes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
